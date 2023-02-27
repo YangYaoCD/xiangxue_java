040ef29507b4945126d3b0e96ac39d95d1873ed2
yangyao <1344262070@qq.com>
2023-02-27 21:47:17 +0800

    20230227 第一次提交

---
+++ src/TestGit.java
@@ -0,0 +1,15 @@
+/**
+ * <b><code>TestGit</code></b>
+ * <p/>
+ * Description 测试Git
+ * <p/>
+ * <b>Creation Time:</b> 2023/2/27 21:46.
+ *
+ * @author YangYao
+ * @since xiangxue_java
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("这是第一次提交！");
+    }
+}
